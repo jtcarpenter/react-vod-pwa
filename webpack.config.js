87231d256bf01e9cd7c839c8b1755c180ff98de6
Jason Carpenter <jtcarpenter@gmail.com>
2017-02-13 17:32:00 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const PATHS = {
+    app: path.join(__dirname, 'src/'),
+    public: path.join(__dirname, 'public')
+};
+
+module.exports = {
+    entry: {
+        app: PATHS.app + '/index.js'
+    },
+    output: {
+        path: PATHS.public,
+        filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        inline: true,
+        port: 3333,
+        contentBase: PATHS.public
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: PATHS.app,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2016', 'es2015', 'react']
+                }
+            }
+        ]
+    }
+}
