b388c2997dba7541c6c60dba4eac33f1e8196b73
Jason Carpenter <jtcarpenter@gmail.com>
2017-03-19 16:47:24 +0000

    removes hello world saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,9 @@
-import {hello} from './hello'
 import {watchLoad as watchLoadGrid} from './grid'
 import {watchLoad as watchLoadItem} from './player'
 
 // Single entry point to start all Sagas at once
 export default function *rootSaga() {
   yield [
-    hello(),
     watchLoadGrid(),
     watchLoadItem()
   ]
