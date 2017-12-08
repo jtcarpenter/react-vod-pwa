c294da133b635792fcc5b38af6c0e5c0acffecd0
Jason Carpenter <jtcarpenter@gmail.com>
2017-12-08 22:08:48 +0000

    adds boundary test

--- src/components/errorBoundary/errorBoundary.test.js
+++ src/components/errorBoundary/errorBoundary.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import ErrorBoundary from 'components/errorBoundary/ErrorBoundary';
+import Error from 'components/error/Error.jsx';
+import * as errorMessages from 'constants/errorMessages';
 
 const errorBoundary = (
     <ErrorBoundary>
@@ -13,4 +15,13 @@ describe('ErrorBoundary', () => {
         const wrapper = shallow(errorBoundary);
         expect(wrapper.exists()).toBe(true);
     })
-})
+});
+
+describe('When an error is caught', () => {
+    const wrapper = shallow(errorBoundary);
+    wrapper.setState({hasError: true});
+    it('should display correct error message', () => {
+        const error = wrapper.find(Error);
+        expect(error.prop('errorMessage')).toBe(errorMessages.ERROR_GENERAL);
+    })
+});
